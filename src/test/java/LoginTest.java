0020f0ab883e9be8eb1ece1f0b29f68f29592630
Isupov Mikhail <mshgn00@gmail.com>
2026-07-18 13:38:29 +0400

    LoginTest is added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -8,34 +8,32 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class LoginTest {
-    // 1. Открыть нужный браузер
-    // 2. Зайти на нужный сайт: saucedemo.com
 
     @Test
-    public void zipCode4digits() {
+    public void login() {
         WebDriver browser = new ChromeDriver();
-        browser.get("https://www.sharelane.com/cgi-bin/register.py");
-        browser.findElement(By.xpath("//*[@name='zip_code']")).sendKeys("1337");
-        browser.findElement(By.xpath("//*[@name='zip_code']")).sendKeys(Keys.CONTROL + "A");
-        browser.findElement(By.xpath("//*[@name='zip_code']")).sendKeys(Keys.BACK_SPACE);
-        browser.findElement(By.xpath("//*[@name='zip_code']")).sendKeys("9999");
-        browser.findElement(By.cssSelector("[value='Continue']")).click();
-        boolean isErrorDisplayed = browser.findElement(By.cssSelector(".error_message")).isDisplayed();
-        String errorMessage = browser.findElement(By.cssSelector(".error_message")).getText();
-
-        assertTrue(isErrorDisplayed);
-        assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
-
-        //    browser.quit();
-
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.xpath("//input[@id='user-name']")).sendKeys("visual_user");
+        browser.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+        browser.findElement(By.cssSelector("#login-button")).click();
+        browser.findElement(By.xpath("//span[@data-test='title']")).isDisplayed();
+        assertTrue(browser.findElement(By.xpath("//span[@data-test='title']")).isDisplayed(), "Products");
+        assertEquals(browser.findElement(By.xpath("//span[@data-test='title']")).getText(), "Products");
+        browser.quit();
     }
 
     @Test
-    public void zipCode5digits() {
+    public void incorrectLogin() {
         WebDriver browser = new ChromeDriver();
-        browser.get("https://www.sharelane.com/cgi-bin/register.py");
-        browser.findElement(By.xpath("//*[@name='zip_code']")).sendKeys("17377");
-        browser.findElement(By.cssSelector("[value='Continue']")).click();
-        browser.findElement(By.cssSelector("[value='Register']")).isDisplayed();
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.xpath("//input[@id='user-name']")).sendKeys("Visual_user");
+        browser.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+        browser.findElement(By.cssSelector("#login-button")).click();
+        boolean isErrorDisplayed = browser.findElement(By.xpath("//h3[@data-test='error']")).isDisplayed();
+        assertTrue(isErrorDisplayed);
+        String errorText = browser.findElement(By.xpath("//h3[@data-test='error']")).getText();
+        assertTrue(isErrorDisplayed);
+        assertEquals(errorText, "Epic sadface: Username and password do not match any user in this service");
+        browser.quit();
     }
 }
